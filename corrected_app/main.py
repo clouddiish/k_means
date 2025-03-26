60ee9bf65c133a54218be4b6f1ca21584f951bcb
Olga Szymczak <olga.szymczaak@gmail.com>
2025-03-26 21:32:38 +0100

    feat: better printing in main

--- corrected_app/main.py
+++ corrected_app/main.py
@@ -4,9 +4,12 @@ from corrected_app.top_baby_names.top_baby_names_loop import top_baby_names_loop
 
 def main():
     clusters, centroids = top_baby_names_loop(
-        no_of_iterations=4, no_of_clusters=4, calculate_distance_function=get_euclidean_distance
+        no_of_iterations=4, no_of_clusters=10, calculate_distance_function=get_euclidean_distance
     )
-    print(f"FINAL CLUSTERS: {clusters}")
+    print(f"FINAL CLUSTERS:")
+    for i, cluster in enumerate(clusters):
+        print(f"CLUSTER NO {i}")
+        print(cluster)
     print(f"FINAL CENTROIDS: {centroids}")
 
 
