8f9bb515bc846ebf40f33731543683589bd536b0
Olga Szymczak <olga.szymczaak@gmail.com>
2025-03-29 23:09:14 +0100

    docs: docstrings for funtions in results_analysis.py

--- corrected_app/general/results_analysis.py
+++ corrected_app/general/results_analysis.py
@@ -2,6 +2,14 @@ from typing import Callable
 
 
 def get_cluster_size(cluster: list) -> int:
+    """Gets the size of a cluster.
+
+    Args:
+        cluster (list): A list of data points representing a cluster.
+
+    Returns:
+        int: The number of data points in the cluster.
+    """
     return len(cluster)
 
 
@@ -11,6 +19,18 @@ def get_avg_distance_from_centroid_in_a_cluster(
     exclude_attributes_ids: list,
     calculate_distance_function: Callable[[list, list, list], list],
 ) -> float:
+    """Calculates the average distance of all data points in a cluster from its centroid.
+
+    Args:
+        cluster (list): A list of data points in the cluster.
+        centroid (list): The centroid of the cluster.
+        exclude_attributes_ids (list): A list of attribute indices to exclude from the distance calculation.
+        calculate_distance_function (Callable[[list, list, list], list]): A function to compute the distance
+            between a data point and the centroid, while excluding specified attributes.
+
+    Returns:
+        float: The average distance of data points from the centroid.
+    """
     distances_sum = 0
     for datapoint in cluster:
         distance = calculate_distance_function(centroid, datapoint, exclude_attributes_ids)
@@ -24,6 +44,18 @@ def get_max_distance_from_centroid_in_a_cluster(
     exclude_attributes_ids: list,
     calculate_distance_function: Callable[[list, list, list], list],
 ) -> float:
+    """Finds the maximum distance of any data point in a cluster from its centroid.
+
+    Args:
+        cluster (list): A list of data points in the cluster.
+        centroid (list): The centroid of the cluster.
+        exclude_attributes_ids (list): A list of attribute indices to exclude from the distance calculation.
+        calculate_distance_function (Callable[[list, list, list], list]): A function to compute the distance
+            between a data point and the centroid, while excluding specified attributes.
+
+    Returns:
+        float: The maximum distance of a data point from the centroid.
+    """
     max_distance = 0
     for datapoint in cluster:
         distance = calculate_distance_function(centroid, datapoint, exclude_attributes_ids)
